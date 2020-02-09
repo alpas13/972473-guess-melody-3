24e36f3504efad212dca44ecdb39547d48e393ba
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-02-09 06:53:58 +0100

    Added end to end test for WelcomeScreen component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import PropTypes from "prop-types";
 
+const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount}/>
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick = {welcomeButtonHandler}
+    />
   );
 };
 
 App.propTypes = {
-  errorsCount: PropTypes.number.isRequired
+  errorsCount: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -5,7 +5,9 @@ import WelcomeScreen from "./welcome-screen";
 it(`Render WelcomeScreen`, () => {
   const tree = renderer
       .create(
-          <WelcomeScreen errorsCount={3}
+          <WelcomeScreen
+            errorsCount={3}
+            onWelcomeButtonClick={() => {}}
           />)
 .toJSON();
   expect(tree).toMatchSnapshot();
