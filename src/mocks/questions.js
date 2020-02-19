18858bf14e25977c6b8a7097bef3106f3dd5c38d
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-02-19 20:29:58 +0100

    Added music files to mocks

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -5,23 +5,23 @@ export default [
     type: `genre`,
     genre: `rock`,
     answers: [{
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://drive.google.com/file/d/10qEkVZATObxZ911SQV4NlM_q1VBeaHF9/view?usp=sharing`,
       genre: `rock`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://drive.google.com/file/d/1pVwmuU4AjXnocxJStlWC7dZvHsP1virQ/view?usp=sharing`,
       genre: `blues`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `jazz`,
+      src: `https://drive.google.com/file/d/1f13Hnw8oiKtujTDh-2CIbkRp2bSlX2wo/view?usp=sharing`,
+      genre: `pop`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `rock`,
+      src: `https://drive.google.com/file/d/12RcEZvtXZajw0PPuSOx4pg65GvMDIy06/view?usp=sharing`,
+      genre: `rap`,
     }],
   }, {
     type: `artist`,
     song: {
-      artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      artist: `Kurt Cobain`,
+      src: `https://drive.google.com/file/d/10qEkVZATObxZ911SQV4NlM_q1VBeaHF9/view?usp=sharing`,
     },
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
