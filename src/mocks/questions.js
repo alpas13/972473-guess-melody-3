2fec7cf678c0a9b5ea063e763aca47a913998e89
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-02-20 08:14:41 +0100

    Changed music links

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -5,33 +5,33 @@ export default [
     type: `genre`,
     genre: `rock`,
     answers: [{
-      src: `https://drive.google.com/file/d/10qEkVZATObxZ911SQV4NlM_q1VBeaHF9/view?usp=sharing`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rock`,
     }, {
-      src: `https://drive.google.com/file/d/1pVwmuU4AjXnocxJStlWC7dZvHsP1virQ/view?usp=sharing`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `blues`,
     }, {
-      src: `https://drive.google.com/file/d/1f13Hnw8oiKtujTDh-2CIbkRp2bSlX2wo/view?usp=sharing`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `pop`,
     }, {
-      src: `https://drive.google.com/file/d/12RcEZvtXZajw0PPuSOx4pg65GvMDIy06/view?usp=sharing`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rap`,
     }],
   }, {
     type: `artist`,
     song: {
       artist: `Kurt Cobain`,
-      src: `https://drive.google.com/file/d/10qEkVZATObxZ911SQV4NlM_q1VBeaHF9/view?usp=sharing`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
     },
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `John Jameson`,
+      artist: `Kurt Cobain`,
     }, {
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jack Daniels`,
+      artist: `Adele`,
     }, {
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jim Beam`,
+      artist: `Madonna`,
     }],
   }
 ];
